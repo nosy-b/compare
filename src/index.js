de64ac6cc90fc89ac199effe6d2c650a1ac4fa0f
nosy-b <heythatsme@gmail.com>
2020-06-05 17:37:47 +0200

    extern module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import * as THREE from '../three';
-import * as THREE from '../node_modules/three/build/three.module.js';
+ import * as THREE from '../src/three.module.js';
+// import * as THREE from '../node_modules/three/build/three.module.js';
 import { ShaderCompare } from '../src/ShaderCompare.js';
 
 /*
